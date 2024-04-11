a6e9693462473b6cf2afb4328fe4cd0d37524bf7
Sparkenstein <zetabytes.pp@gmail.com>
2024-04-11 19:00:22 +0530

    feat: use webp

--- assets/index.tsx
+++ assets/index.tsx
@@ -1,9 +1,9 @@
-import Image1 from "./image_1.png";
-import Image2 from "./image_2.png";
-import Image3 from "./image_3.png";
-import Image4 from "./image_4.png";
-import Image5 from "./image_5.png";
-import Image6 from "./image_6.png";
-import Image7 from "./image_7.png";
+import Image1 from "./image_1.webp";
+import Image2 from "./image_2.webp";
+import Image3 from "./image_3.webp";
+import Image4 from "./image_4.webp";
+import Image5 from "./image_5.webp";
+import Image6 from "./image_6.webp";
+import Image7 from "./image_7.webp";
 
 export { Image1, Image2, Image3, Image4, Image5, Image6, Image7 };
